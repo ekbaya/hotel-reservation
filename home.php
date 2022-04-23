2be3c31a51e6d7a51fbb1a9fa938c49449a0ae23
ekbaya <Eliasbaya1223@gmail.com>
2022-04-23 19:15:23 +0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,335 @@
+<div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Welcome</div>
+            <h1>Golden Palm Breeze Hotel</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row intro_row">
+        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+          <div class="intro_text text-center">
+            <p>Golden Palm Breeze Hotel is a modern, upscale hospitality destination that is passionate about ‘making moments’,recognizing that small gestures make a big difference to our guests. We do ordinary things in an extraordinary way – a philosophy that has defined our brand’s success from the very start.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row gallery_row">
+        <div class="col">
+          <!-- Gallery -->
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">
+              
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_4.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_right container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6 order-xl-1 order-2">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- Rooms Content -->
+        <div class="col-xl-6 order-xl-2 order-1">
+          <div class="rooms_right_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Double Twin</h1>
+            </div>
+            <div class="rooms_text">
+              <p>This room has two double eds And It Is Very Spacious, Suitable for 2 Families. It has  flat screen TV and electric fireplace.  Wifi, Shower,Toilet,Daily Housekeeping,  Room Service, Curtains ,Iron, Air Conditioner, Telephone, Breakfast The entire suite is designed in a fashion to allow mobility impaired accessibility. Room offer comfort and privacy with modern amenities that will make your stay productive and peaceful. It has tastefully appointed rooms with twin bed or a choice of large double bed and is fitted with a writing desk.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Free Wi-Fi</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Coffee Machine</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Room Service</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">KES 8500<span>Night</span></div>
+           
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_left container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+        
+        <!-- Rooms Content -->
+        <div class="col-xl-6">
+          <div class="rooms_left_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Super Deluxe</h1>
+            </div>
+            <div class="rooms_text">
+              <p>Spacious than deluxe rooms. It has  flat screen TV and electric fireplace.  Wifi, Shower, Sofa, Toilet,Daily Housekeeping,  Room Service, Curtains ,towel,  Iron, Air Conditioner, Telephone, Breakfast,TVThe entire suite is designed in a fashion to allow mobility impaired accessibility.Room offer comfort and privacy with modern amenities that will make your stay productive and peaceful. It has tastefully appointed rooms with twin bed or a choice of large double bed and is fitted with a writing desk.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Free Wi-Fi</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>King Size Bed</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Bath Tub</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">KES 8000/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover -->
+
+  <div class="discover">
+
+    <!-- Discover Content -->
+    <div class="discover_content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <div class="section_title">
+              <div>Hotel</div>
+              <h1>Discover Golden Palm Breeze Hotel</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row discover_row">
+          <div class="col-lg-5">
+            <div class="discover_highlight">
+              <p>Golden Palm Breeze Hotel is located in Kabati. Local points of interest include Christina Wangari Gardens and Thika Municipal Stadium.</p>
+            </div>
+            <!-- <div class="button discover_button"><a href="#">discover</a></div> -->
+          </div>
+          <div class="col-lg-7">
+            <div class="discover_text">
+              <p>3.5-star hotel with free breakfast and fitness center An outdoor pool, a restaurant, and a fitness center are available at this hotel. Free buffet breakfast and free self parking are also provided. Additionally, a bar/lounge, a poolside bar, and a 24-hour front desk are onsite. All 155 rooms offer free WiFi, room service, and LCD TVs with cable channels. Free bottled water, showers, and desks are among the other amenities that guests will find.Golden Palm Breeze Hotel offers 155 accommodations with complimentary bottled water and slippers. 32-inch LCD televisions come with cable channels. Bathrooms include showers and complimentary toiletries. Guests can surf the web using the complimentary wireless Internet access. Housekeeping is provided daily. Recreational amenities at the hotel include an outdoor pool and a fitness center. The recreational activities listed below are available either on site or nearby; fees may apply.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Discover Slider -->
+    <div class="discover_slider_container">
+      <div class="owl-carousel owl-theme discover_slider">
+        
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Weddings</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Parties</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Relax</a></h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Testimonials -->
+
+  <div class="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Clients</div>
+            <h1>Testimonials</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row testimonials_row">
+        <div class="col">
+          
+          <!-- Testimonials Slider -->
+          <div class="testimonials_slider_container">
+            <div class="owl-carousel owl-theme testimonials_slider">
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>I had a wonderful experience at the (HN). Every staff member I encountered, from the valet to the check- in to the cleaning staff were delightful and eager to help! Thank you! Will recommend to my colleagues!.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Elias Baya,</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>We vistited this hotel the last mont. The staff were very great and quality sevice, so, the eat is varity and excelent , also the rooms were cleand and confort bed. it has afantastic location for the people that visit the towon for work. Will recomendet thes hotel for all persons that to travel this city.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Lydiah Wacera,</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>The rooms were clean, very comfortable, and the staff was amazing. They went over and beyond to help make our stay enjoyable. I highly recommend this hotel for anyone visiting here.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Samuel Baraka,</a><span> Client</span></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
